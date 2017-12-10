fc9f658c0f1f6507b58bf13e1b0b04914521370c
Hardik Patel <hardikgw@gmail.com>
2017-12-10 22:24:56 +0530

    notebook

---
+++ README.md
@@ -0,0 +1,3 @@
+##ElasticSearch Classifier Using TensorFlow
+
+
--- src/main/python/Trainer.py
+++ src/main/python/Trainer.py
@@ -1,2 +1,83 @@
-import tensorflow as tf
-import elasticsearch as es
+import pandas as pd # provide sql-like data manipulation tools. very handy.
+pd.options.mode.c
+hained_assignment = None
+import numpy as np # high dimensional vector computing library.
+from copy import deepcopy
+from string import punctuation
+from random import shuffle
+
+import gensim
+from gensim.models.word2vec import Word2Vec # the word2vec model gensim class
+LabeledSentence = gensim.models.doc2vec.LabeledSentence # we'll talk about this down below
+
+from tqdm import tqdm
+tqdm.pandas(desc="progress-bar")
+
+from nltk.tokenize import TweetTokenizer # a tweet tokenizer from nltk.
+tokenizer = TweetTokenizer()
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def ingest():
+    data = pd.read_csv('./tweets.csv')
+    data.drop(['ItemID', 'SentimentSource'], axis=1, inplace=True)
+    data = data[data.Sentiment.isnull() == False]
+    data['Sentiment'] = data['Sentiment'].map(int)
+    data = data[data['SentimentText'].isnull() == False]
+    data.reset_index(inplace=True)
+    data.drop('index', axis=1, inplace=True)
+    print 'dataset loaded with shape', data.shape    
+    return data
+
+data = ingest()
+data.head(5)
+
+def tokenize(tweet):
+    try:
+        tweet = unicode(tweet.decode('utf-8').lower())
+        tokens = tokenizer.tokenize(tweet)
+        tokens = filter(lambda t: not t.startswith('@'), tokens)
+        tokens = filter(lambda t: not t.startswith('#'), tokens)
+        tokens = filter(lambda t: not t.startswith('http'), tokens)
+        return tokens
+    except:
+        return 'NC'
+
+def postprocess(data, n=1000000):
+    data = data.head(n)
+    data['tokens'] = data['SentimentText'].progress_map(tokenize)  ## progress_map is a variant of the map function plus a progress bar. Handy to monitor DataFrame creations.
+    data = data[data.tokens != 'NC']
+    data.reset_index(inplace=True)
+    data.drop('index', inplace=True, axis=1)
+    return data
+
+data = postprocess(data)
+
+x_train, x_test, y_train, y_test = train_test_split(np.array(data.head(n).tokens),
+                                                    np.array(data.head(n).Sentiment), test_size=0.2)
+
+
+def labelizeTweets(tweets, label_type):
+    labelized = []
+    for i,v in tqdm(enumerate(tweets)):
+        label = '%s_%s'%(label_type,i)
+        labelized.append(LabeledSentence(v, [label]))
+    return labelized
+
+x_train = labelizeTweets(x_train, 'TRAIN')
+x_test = labelizeTweets(x_test, 'TEST')
+
+x_train[0]
+
+Out[13]:
+TaggedDocument(words=[u'thank', u'you', u'!', u'im', u'just', u'a', u'tad', u'sad', u'u', u'r', u'off', u'the', u'market', u'tho', u'...'], tags=['TRAIN_0'])
+
+
+tweet_w2v = Word2Vec(size=n_dim, min_count=10)
+tweet_w2v.build_vocab([x.words for x in tqdm(x_train)])
+tweet_w2v.train([x.words for x in tqdm(x_train)])
+
+
+tweet_w2v['good']
+
