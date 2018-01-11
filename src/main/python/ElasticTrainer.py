9da1e8852cc2f45e2eda65bf24055d5edf1062ec
Hardik Patel <hardikgw@gmail.com>
2018-01-11 14:04:02 -0500

    elastic trainer

---
+++ src/main/python/ElasticTrainer.py
@@ -0,0 +1,48 @@
+from elasticsearch import Elasticsearch
+
+import nltk
+nltk.download('punkt')
+
+from nltk.tokenize import word_tokenize
+
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
+
+from nltk.stem.porter import PorterStemmer
+porter = PorterStemmer()
+
+es = Elasticsearch(["elasticsearch"], maxsize=25)
+
+def tokenize(text):
+    try:
+        tokens = word_tokenize(text)
+        tokens = [w.lower() for w in tokens]
+        import string
+        table = str.maketrans('', '', string.punctuation)
+        stripped = [w.translate(table) for w in tokens]
+        words = [word for word in stripped if word.isalpha()]
+        words = [w for w in words if not w in stop_words]
+        return words
+    except:
+        return 'NC'
+
+def tokens():
+    tokens = []
+    res = es.search(index="intelligence", body={"query": {"match_all": {}}})
+    print("Got %d Hits:" % res['hits']['total'])
+    for hit in res['hits']['hits']:
+        # print("%(category)s %(text)s" % hit["_source"])
+        text = hit["_source"]["text"][0]
+        text = text.replace('\\n', ' ')
+        tokens.append(tokenize(text))
+    return tokens
+
+tokens = tokens()
+
+import gensim
+model = gensim.models.Word2Vec(tokens, size=100)
+
+model.most_similar(positive=['algorithm'])
+
+w2v = dict(zip(model.wv.index2word, model.wv.syn0))
