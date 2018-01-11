04c9d7538ce31e347b09707397d2e46900514c52
Hardik Patel <hardikgw@gmail.com>
2018-01-11 12:59:37 -0500

    token stemming, punctuations

--- src/main/python/Elastic.py
+++ src/main/python/Elastic.py
@@ -5,6 +5,13 @@ nltk.download('punkt')
 
 from nltk.tokenize import word_tokenize
 
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
+
+from nltk.stem.porter import PorterStemmer
+porter = PorterStemmer()
+
 es = Elasticsearch(["elasticsearch"], maxsize=25)
 
 
@@ -13,5 +20,11 @@ print("Got %d Hits:" % res['hits']['total'])
 for hit in res['hits']['hits']:
     # print("%(category)s %(text)s" % hit["_source"])
     text = hit["_source"]["text"][0]
-    words = word_tokenize(text)
+    tokens = word_tokenize(text)
+    tokens = [w.lower() for w in tokens]
+    import string
+    table = str.maketrans('', '', string.punctuation)
+    stripped = [w.translate(table) for w in tokens]
+    words = [word for word in tokens if word.isalpha()]
+    words = [w for w in words if not w in stop_words]
     print(words)
