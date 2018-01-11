ce9195c316f1ac9edf44703601c26397435e0664
Hardik Patel <hardikgw@gmail.com>
2018-01-11 12:32:40 -0500

    word punctuations

--- src/main/python/Elastic.py
+++ src/main/python/Elastic.py
@@ -1,7 +1,17 @@
 from elasticsearch import Elasticsearch
+
+import nltk
+nltk.download('punkt')
+
+from nltk.tokenize import word_tokenize
+
 es = Elasticsearch(["elasticsearch"], maxsize=25)
+
 
 res = es.search(index="intelligence", body={"query": {"match_all": {}}})
 print("Got %d Hits:" % res['hits']['total'])
 for hit in res['hits']['hits']:
-    print("%(category)s %(text)s" % hit["_source"])
+    # print("%(category)s %(text)s" % hit["_source"])
+    text = hit["_source"]["text"][0]
+    words = word_tokenize(text)
+    print(words)
