4226ac9949ddc1279abc308d3ae3cb6f072f8e0a
Hardik Patel <hardikgw@gmail.com>
2018-01-31 12:51:24 -0500

    checkpoint checkin

--- src/main/python/ElasticSimilarity.py
+++ src/main/python/ElasticSimilarity.py
@@ -3,71 +3,81 @@ import nltk
 import gensim
 import os
 import re
-
+from gensim.models.doc2vec import TaggedDocument
 nltk.download('punkt')
-
-START_DOC = 5
-TRAIN_DOCS = 50
-
 from nltk.tokenize import word_tokenize
-
+import string
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 stop_words = set(stopwords.words('english'))
 
 from nltk.stem.porter import PorterStemmer
 porter = PorterStemmer()
-from gensim.models.doc2vec import TaggedDocument
 
-es = Elasticsearch(["elasticsearch"], maxsize=25)
+START_DOC = 5
+TRAIN_DOCS = 50
 
-taggeddoc = []
+class ElasticSimilarity:
 
-def es_docs():
-    res = es.search(index="intelligence", size=TRAIN_DOCS, body={"query": {"match_all": {}}})
-    print("Got %d Hits:" % res['hits']['total'])
-    for hit in res['hits']['hits']:
-        # print("%(category)s %(text)s" % hit["_source"])
-        text = hit["_source"]["text"][0]
-        id = hit["_id"]
-        yield text, id
+    def __init__(self):
 
-def clean_tokens(text):
-    try:
-        tokens = word_tokenize(text)
-        tokens = [w.lower() for w in tokens]
-        import string
-        table = str.maketrans('', '', string.punctuation)
-        stripped = [w.translate(table) for w in tokens]
-        words = [word for word in stripped if word.isalpha()]
-        words = [w for w in words if not w in stop_words]
-        return words
-    except:
-        return 'NC', -1
+        self.base_dir = '/assets'
+        self.model_file = os.path.join(self.base_dir, 'doc_model')
+        #open(os.path.join(GLOVE_DIR, 'glove.6B.100d.txt'), 'rt', encoding='utf8')
 
-def train():
-    for doc, id in es_docs():
-        tokens = clean_tokens(doc)
-        td = TaggedDocument(gensim.utils.to_unicode(str.encode(' '.join(tokens))).split(),str(id))
-        taggeddoc.append(td)
+        self.es = Elasticsearch(["elasticsearch"], maxsize=25)
 
-    print ('Data Loading finished')
-    print (len(taggeddoc),type(taggeddoc))
+        self.taggeddoc = []
 
-    model = gensim.models.Doc2Vec(taggeddoc, dm = 0, alpha=0.025, size= 20, min_alpha=0.025, min_count=0)
-    # start training
-    for epoch in range(200):
-        if epoch % 20 == 0:
-            print ('Now training epoch %s'%epoch)
-        model.train(taggeddoc,total_examples=model.corpus_count,epochs=model.iter)
-        model.alpha -= 0.002  # decrease the learning rate
-        model.min_alpha = model.alpha  # fix the learning rate, no decay
+    def es_docs(self, start_doc, train_docs):
+        res = self.es.search(index="intelligence", from_=start_doc, size=train_docs, body={"query": {"match_all": {}}})
+        print("Got %d Hits:" % res['hits']['total'])
+        for hit in res['hits']['hits']:
+            # print("%(category)s %(text)s" % hit["_source"])
+            text = hit["_source"]["text"][0]
+            text = text.replace('\\n', ' ')
+            id = hit["_id"]
+            yield text, id
 
-    # shows the similar words
-    print (model.most_similar('suppli'))
+    def clean_tokens(self, text):
+        try:
+            tokens = word_tokenize(text)
+            tokens = [w.lower() for w in tokens]
+            table = str.maketrans('', '', string.punctuation)
+            stripped = [w.translate(table) for w in tokens]
+            words = [word for word in stripped if word.isalpha()]
+            words = [w for w in words if (len(w) > 2 and not w in stop_words)]
+            words = [porter.stem(w) for w in words]
+            return words
+        except:
+            return 'NC'
 
-    # shows the learnt embedding
-    print (model['suppli'])
+    def train(self, start_doc, train_docs):
+        for doc, id in self.es_docs(start_doc, train_docs):
+            tokens = self.clean_tokens(doc)
+            if tokens != 'NC':
+                td = TaggedDocument(gensim.utils.to_unicode(str.encode(' '.join(tokens))).split(),str(id))
+                self.taggeddoc.append(td)
+
+        print ('Data Loading finished')
+        print (len(self.taggeddoc),type(self.taggeddoc))
+
+        model = gensim.models.Doc2Vec(self.taggeddoc, dm = 0, alpha=0.025, size= 20, min_alpha=0.025, min_count=0)
+        # start training
+        for epoch in range(200):
+            if epoch % 20 == 0:
+                print ('Now training epoch %s'%epoch)
+            model.train(self.taggeddoc,total_examples=model.corpus_count,epochs=model.iter)
+            model.alpha -= 0.002  # decrease the learning rate
+            model.min_alpha = model.alpha  # fix the learning rate, no decay
+
+        # shows the similar words
+        print (model.most_similar('suppli'))
+
+        # shows the learnt embedding
+        print (model['suppli'])
+
 
 if __name__ == '__main__':
-    train()
+    esSimilarity = ElasticSimilarity()
+    esSimilarity.train(START_DOC, TRAIN_DOCS)
