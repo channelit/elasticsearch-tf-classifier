9e8e1fc51c51e2a196a04d3b2e6ed256335e276a
Hardik Patel <hardikgw@gmail.com>
2018-02-02 14:42:34 -0500

    refactor

--- src/main/python/ElasticSimilarity.py
+++ src/main/python/ElasticSimilarity.py
@@ -3,7 +3,6 @@ from _config import ConfigMap
 import nltk
 import gensim
 import os
-import re
 from gensim.models.doc2vec import TaggedDocument
 nltk.download('punkt')
 from nltk.tokenize import word_tokenize
@@ -15,7 +14,7 @@ stop_words = set(stopwords.words('english'))
 from nltk.stem.porter import PorterStemmer
 porter = PorterStemmer()
 
-TRAIN_DOCS = 5
+TRAIN_DOCS = 15
 es = ConfigMap("ElasticSearch")
 training = ConfigMap("Training")
 
@@ -28,18 +27,6 @@ class ElasticSimilarity:
         self.es = Elasticsearch([es['server']], port = es['port'])
 
         self.taggeddoc = []
-
-    def es_docs(self, train_docs):
-        res = helpers.scan(index=es['index'], size=train_docs, scroll='1m', client = self.es, preserve_order=True,
-                           query={"query": {"match_all": {}}},
-                           )
-        for hit in res:
-            if "text" in hit["_source"] :
-                # print("%(category)s %(text)s" % hit["_source"])
-                text = hit["_source"][es['textfield']][0]
-                text = text.replace('\\n', ' ')
-                id = hit["_id"]
-                yield text, id
 
 
     def es_doc(self, doc_id):
@@ -60,27 +47,6 @@ class ElasticSimilarity:
             return words
         except:
             return 'NC'
-
-    def train(self, train_docs):
-        for doc, id in self.es_docs(train_docs):
-            tokens = self.clean_tokens(doc)
-            if tokens != 'NC':
-                td = TaggedDocument(gensim.utils.to_unicode(str.encode(' '.join(tokens))).split(),[id])
-                self.taggeddoc.append(td)
-
-        print ('Data Loading finished')
-        print (len(self.taggeddoc),type(self.taggeddoc))
-
-        model = gensim.models.Doc2Vec(self.taggeddoc, dm = 0, alpha=0.025, size= 20, min_alpha=0.025, min_count=0)
-
-        for epoch in range(200):
-            if epoch % 20 == 0:
-                print ('Now training epoch %s'%epoch)
-            model.train(self.taggeddoc,total_examples=model.corpus_count,epochs=model.iter)
-            model.alpha -= 0.002  # decrease the learning rate
-            model.min_alpha = model.alpha  # fix the learning rate, no decay
-        model.save(self.model_file)
-        model.save_word2vec_format(self.model_file + '.word2vec')
 
     def similar(self, doc_id):
         word2vec = gensim.models.KeyedVectors.load_word2vec_format(self.model_file + '.word2vec')
--- src/main/python/ElasticTrainer.py
+++ src/main/python/ElasticTrainer.py
@@ -1,56 +1,95 @@
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, helpers
+from _config import ConfigMap
 import nltk
-
-# nltk.download('punkt')
-
+import gensim
+import os
+from gensim.models.doc2vec import TaggedDocument
+nltk.download('punkt')
 from nltk.tokenize import word_tokenize
-
-# nltk.download('stopwords')
+import string
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 stop_words = set(stopwords.words('english'))
 
 from nltk.stem.porter import PorterStemmer
 porter = PorterStemmer()
 
-es = Elasticsearch(["elasticsearch"], maxsize=25)
+TRAIN_DOCS = 15
+es = ConfigMap("ElasticSearch")
+training = ConfigMap("Training")
 
-def tokenize(text):
-    try:
-        tokens = word_tokenize(text)
-        tokens = [w.lower() for w in tokens]
-        import string
-        table = str.maketrans('', '', string.punctuation)
-        stripped = [w.translate(table) for w in tokens]
-        words = [word for word in stripped if word.isalpha()]
-        words = [w for w in words if not w in stop_words]
-        return words
-    except:
-        return 'NC'
+class ElasticSimilarity:
 
-def docs():
-    docs = []
-    res = es.search(index="intelligence", size=15, body={"query": {"match_all": {}}})
-    print("Got %d Hits:" % res['hits']['total'])
-    for hit in res['hits']['hits']:
-        # print("%(category)s %(text)s" % hit["_source"])
-        text = hit["_source"]["text"][0]
+    def __init__(self):
+
+        self.model_file = os.path.join(training['basedir'], 'doc_model')
+
+        self.es = Elasticsearch([es['server']], port = es['port'])
+
+        self.taggeddoc = []
+
+    def es_docs(self, train_docs):
+        res = helpers.scan(index=es['index'], size=train_docs, scroll='1m', client = self.es, preserve_order=True,
+                           query={"query": {"match_all": {}}},
+                           )
+        for hit in res:
+            if "text" in hit["_source"] :
+                # print("%(category)s %(text)s" % hit["_source"])
+                text = hit["_source"][es['textfield']][0]
+                text = text.replace('\\n', ' ')
+                id = hit["_id"]
+                yield text, id
+
+
+    def es_doc(self, doc_id):
+        res = self.es.get(index=es['index'], id=doc_id, doc_type=es['type'])
+        text = res["_source"][es['textfield']][0]
         text = text.replace('\\n', ' ')
-        docs.append(text)
-    return docs
+        return text
 
-def tokens(docs):
-    tokens = []
-    for doc in docs:
-        tokens.append(tokenize(doc))
-    return tokens
+    def clean_tokens(self, text):
+        try:
+            tokens = word_tokenize(text)
+            tokens = [w.lower() for w in tokens]
+            table = str.maketrans('', '', string.punctuation)
+            stripped = [w.translate(table) for w in tokens]
+            words = [word for word in stripped if word.isalpha()]
+            words = [w for w in words if (len(w) in range(2,12) and not w in stop_words)]
+            words = [porter.stem(w) for w in words]
+            return words
+        except:
+            return 'NC'
 
-docs = docs()
-tokens = tokens(docs)
+    def train(self, train_docs):
+        for doc, id in self.es_docs(train_docs):
+            tokens = self.clean_tokens(doc)
+            if tokens != 'NC':
+                td = TaggedDocument(gensim.utils.to_unicode(str.encode(' '.join(tokens))).split(),[id])
+                self.taggeddoc.append(td)
 
-import gensim
-model = gensim.models.Word2Vec(tokens, size=100)
+        print ('Data Loading finished')
+        print (len(self.taggeddoc),type(self.taggeddoc))
 
-model.most_similar(positive=['method'])
+        model = gensim.models.Doc2Vec(self.taggeddoc, dm = 0, iter=1,  window=5, seed=1337, min_count=5, workers=4, alpha=0.025, size= 20, min_alpha=0.025)
 
-w2v = model.wv
-# w2v = dict(zip(model.wv.index2word, model.wv.syn0))
+        for epoch in range(200):
+            if epoch % 20 == 0:
+                print ('Now training epoch %s'%epoch)
+            model.train(self.taggeddoc,total_examples=model.corpus_count,epochs=model.iter)
+            model.alpha -= 0.002  # decrease the learning rate
+            model.min_alpha = model.alpha  # fix the learning rate, no decay
+        model.save(self.model_file)
+        model.save_word2vec_format(self.model_file + '.word2vec')
+
+    def similar(self, doc_id):
+        word2vec = gensim.models.KeyedVectors.load_word2vec_format(self.model_file + '.word2vec')
+        model = gensim.models.Doc2Vec.load(self.model_file)
+        doc = self.es_doc(doc_id)
+        tokens = self.clean_tokens(doc)
+        infer = model.infer_vector(tokens)
+        similar = model.docvecs.most_similar([infer])
+        print(similar)
+
+if __name__ == '__main__':
+    esSimilarity = ElasticSimilarity()
+    esSimilarity.train(TRAIN_DOCS)
