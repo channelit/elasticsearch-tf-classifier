e0fadf35cc931a7d6f8cb4a3213b4a871c26e6eb
Hardik Patel <hardikgw@gmail.com>
2018-02-07 08:54:08 -0500

    Trajectory checkpoint

--- src/main/python/Trajectory.py
+++ src/main/python/Trajectory.py
@@ -1,5 +1,9 @@
 from pysal.cg.shapes import Point
+from pysal.cg.shapes import Chain
 import pysal
+
+MAX_LINES = 1000
+NUM_GROUPS = 100
 
 class Trajectory:
 
@@ -22,17 +26,39 @@ if __name__ == '__main__':
 
         linectr = 0
         for row in readCSV:
-            if 0 < linectr < 100:
-                p_start = Point((row[6],row[5]))
-                p_end = Point((row[10],row[9]))
-                start_pos.append(p_start)
-                end_pos.append(p_end)
-            if linectr > 100:
+            if 0 < linectr < MAX_LINES:
+                p_start = Point((float(row[6]),float(row[5])))
+                p_end = Point((float(row[10]),float(row[9])))
+                if not p_start in start_pos:
+                    start_pos.append(p_start)
+                if not p_end in end_pos:
+                    end_pos.append(p_end)
+            if linectr > MAX_LINES:
                 break
             linectr+=1
 
-    wknn3 = pysal.weights.KNN(start_pos)
+    # tree = trajectory.GenerateTree(start_pos)
 
-    tree = trajectory.GenerateTree(start_pos)
-    print(start_pos)
-    print(end_pos)
+    knn_start = pysal.weights.KNN(start_pos, k = NUM_GROUPS)
+    knn_end = pysal.weights.KNN(end_pos, k = NUM_GROUPS)
+
+    start_groups = []
+    start_group = []
+    end_groups = []
+
+    for n in knn_start.neighbors:
+        start_group = []
+        for i in knn_start.neighbors[n]:
+            start_group.append(start_pos[i])
+        start_groups.append(start_group)
+
+    for n in knn_end.neighbors:
+        end_group= []
+        for i in knn_end.neighbors[n]:
+            end_group.append(end_pos[i])
+        end_groups.append(end_group)
+
+    for g in start_groups:
+        c = Chain(g)
+        print(c.bounding_box)
+    print('done')
