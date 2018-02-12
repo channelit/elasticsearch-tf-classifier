af60d3f98faffde8bc7a3f424b15bd1094215e13
Hardik Patel <hardikgw@gmail.com>
2018-02-12 13:07:07 -0500

    text cleaner using SpaCy

--- src/main/python/TextCleaner.py
+++ src/main/python/TextCleaner.py
@@ -6,24 +6,22 @@ import numpy as np
 
 class TextCleaner:
 
-    my_stop_words = [u'say', u'\'s', u'Mr', u'be', u'said', u'says', u'saying']
-    nlp = spacy.load("en")
+    def __init__(self):
+        my_stop_words = [u'say', u'\'s', u'Mr', u'be', u'said', u'says', u'saying']
+        self.nlp = spacy.load("en")
 
-    for stopword in my_stop_words:
-        lexeme = nlp.vocab[stopword]
-        lexeme.is_stop = True
+        for stopword in my_stop_words:
+            lexeme = self.nlp.vocab[stopword]
+            lexeme.is_stop = True
 
-    def __init__(self, text):
-        self.text = text
-
-    def unicode(text):
-        return unicode(''.join([i if ord(i) < 128 else ' ' for i in text]))
+    def unicode(self, text):
+        return ''.join([i if ord(i) < 128 else ' ' for i in text])
 
     def nlp_text(self, text):
         return self.nlp(self.unicode(text))
 
-    def clean_tokens(self):
-        nlp_text = self.nlp_text(self.text)
+    def clean_tokens(self, text):
+        nlp_text = self.nlp_text(text)
         tokens = [w for w in nlp_text if w.text != '\n' and not w.is_stop and not w.is_punct and not w.like_num]
         bigram = gensim.models.Phrases(tokens)
         return [bigram[line] for line in tokens]
