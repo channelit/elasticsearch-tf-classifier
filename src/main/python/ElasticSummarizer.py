de80f7300d591a01f8ef4e56fbbc14f3ed8908b9
Hardik Patel <hardikgw@gmail.com>
2018-03-05 13:07:08 -0500

    cleanup

--- src/main/python/ElasticSummarizer.py
+++ src/main/python/ElasticSummarizer.py
@@ -1,24 +1,25 @@
 from elasticsearch import Elasticsearch, helpers
 from _config import ConfigMap
-import nltk
+# import nltk
 from gensim.summarization import summarize
 from gensim.summarization import keywords
 import os
 import numpy
 import json
-
-nltk.download('punkt')
-from nltk.tokenize import word_tokenize
 import string
+from TextCleaner import TextCleaner
 
-nltk.download('stopwords')
-from nltk.corpus import stopwords
+# nltk.download('punkt')
+# from nltk.tokenize import word_tokenize
+#
+# nltk.download('stopwords')
+# from nltk.corpus import stopwords
+#
+# stop_words = set(stopwords.words('english'))
+# from nltk.stem.porter import PorterStemmer
+# porter = PorterStemmer()
 
-stop_words = set(stopwords.words('english'))
-
-from nltk.stem.porter import PorterStemmer
-
-porter = PorterStemmer()
+text_cleaner = TextCleaner()
 
 BATCH_SIZE = 15
 es = ConfigMap("ElasticSearch")
@@ -46,10 +47,7 @@ class ElasticSummarizer:
                 }
             }
         }
-        res = helpers.scan(index=es['index'], size=BATCH_SIZE, scroll='1m',
-                           client=self.es, preserve_order=True,
-                           query=eval(query['summary']),
-                           )
+        res = helpers.scan(index=es['index'], size=BATCH_SIZE, scroll='1m', client=self.es, preserve_order=True, query=eval(query['summary']))
         ctr = 0
         for hit in res:
             ctr += 1
@@ -60,17 +58,16 @@ class ElasticSummarizer:
             if es['textfield'] in hit["_source"]:
                 text = eval(es['textfieldobj'])
                 text = text.replace('\\n', ' ').replace('\\t', ' ')
-                text = ''.join([x if x.isalpha() or x.isspace() else " " for x in text])
-                text = text.strip()
+                text = '. '.join(text_cleaner.clean_sentences(text))
                 id = hit["_id"]
                 if len(text) > 100:
                     yield text, id
 
-    def clean_tokens(self, tokens):
-        tokens = [w.lower() for w in tokens]
-        tokens = [t for t in tokens if t.isalpha() and t not in string.punctuation]
-        tokens = [porter.stem(t) for t in tokens]
-        return numpy.unique(tokens)
+    # def clean_tokens(self, tokens):
+    #     tokens = [w.lower() for w in tokens]
+    #     tokens = [t for t in tokens if t.isalpha() and t not in string.punctuation]
+    #     tokens = [porter.stem(t) for t in tokens]
+    #     return numpy.unique(tokens)
 
     def populate_summaries(self):
         for text, id in self.es_docs():
@@ -78,11 +75,11 @@ class ElasticSummarizer:
             try:
                 text_summary = summarize(text, ratio=0.8)
                 text_keywords = keywords(text, ratio=0.8)
-                text_keywords = self.clean_tokens(text_keywords.splitlines())
+                text_keywords = text_cleaner.clean_tokens(text_keywords)
                 body = {
                     "doc": {
                         "text_summary": text_summary,
-                        "text_keywords": text_keywords.tolist()
+                        "text_keywords": text_keywords
                     }
                 }
                 update_response = self.es.update(index=es['index'], doc_type=es['type'], body=body, id=id,
