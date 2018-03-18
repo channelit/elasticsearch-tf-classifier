127650015e62e0777f7c973c59ffba50076dd4c8
Hardik Patel <hardikgw@gmail.com>
2018-03-17 20:02:38 -0400

    config relocate

--- src/main/resources/commands.sh
+++ src/main/resources/commands.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 bash <(curl -s https://raw.githubusercontent.com/channelit/launch-scripts/master/aws/docker_ubuntu.sh)
 docker cp ../python python:/src
+docker exec python python /src/Trajectory.py
