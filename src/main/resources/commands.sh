4577aac5283e8451f5a21e3b0d37dc7d0587f258
Hardik Patel <hardikgw@gmail.com>
2018-03-21 11:31:57 -0400

    version

--- src/main/resources/commands.sh
+++ src/main/resources/commands.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 bash <(curl -s https://raw.githubusercontent.com/channelit/launch-scripts/master/aws/docker_ubuntu.sh)
-git lfs fetch
+
+# for re-fetch lfs
+# git lfs fetch
 docker cp ../python python:/src
-docker exec python python /src/Trajectory.py
-scp -i "cit.pem" -r ubuntu@ec2-54-209-39-87.compute-1.amazonaws.com:/home/ubuntu/elasticsearch-tf-classifier/src/main/resources/data/tensorflow .
+docker exec python python /src/Trajectory.py -d
+scp -i "cit.pem" -r ubuntu@ec2-54-175-79-187.compute-1.amazonaws.com:/home/ubuntu/elasticsearch-tf-classifier/src/main/resources/data/tensorflow .
